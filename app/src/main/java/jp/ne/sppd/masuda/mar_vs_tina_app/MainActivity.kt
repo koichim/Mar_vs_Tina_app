52bbd10efb7b84aa46730ed9f0a8912a53a3ff29
KOHICHI MASUDA <KOICHIM@jp.ibm.com>
2024-08-08 00:44:59 +0900

    1st commit with large file

---
+++ app/src/main/java/jp/ne/sppd/masuda/mar_vs_tina_app/MainActivity.kt
@@ -0,0 +1,243 @@
+package jp.ne.sppd.masuda.mar_vs_tina_app
+// TenorFlow Lite 入門 / Androidによる画像分類 - https://note.com/npaka/n/n968c3bb9ea8f
+//import kotlinx.android.synthetic.main.activity_main.*
+
+import android.Manifest
+import android.R
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.media.MediaActionSound
+import android.media.SoundPool
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.OrientationEventListener
+import android.view.Surface
+import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.*
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import jp.ne.sppd.masuda.mar_vs_tina_app.databinding.ActivityMainBinding
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+
+typealias LumaListener = (luma: Double) -> Unit
+
+class MainActivity : AppCompatActivity() {
+    private var thisMainActivity = this
+    private lateinit var binding: ActivityMainBinding
+    private var preview: Preview? = null
+    private var imageCapture: ImageCapture? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+
+    private lateinit var soundPool: SoundPool
+    private var soundOne = 0
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults:
+        IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //    setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        // Setup the listener for take photo button
+        binding.cameraCaptureButton.setOnClickListener { takePhoto() }
+
+        //outputDirectory = getOutputDirectory()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+//        binding.viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+//            updateTransform()
+//        }
+    }
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            preview = Preview.Builder()
+                .build()
+
+            //image
+            imageCapture = ImageCapture.Builder()
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                //    .setTargetResolution(Size(previewWidth, previewHeight))
+                .build()
+
+            val orientationEventListener = object : OrientationEventListener(this as Context) {
+                override fun onOrientationChanged(orientation: Int) {
+                    // Monitors orientation values to determine the target rotation value
+                    val rotation: Int = when (orientation) {
+                        in 45..134 -> Surface.ROTATION_270
+                        in 135..224 -> Surface.ROTATION_180
+                        in 225..314 -> Surface.ROTATION_90
+                        else -> Surface.ROTATION_0
+                    }
+
+                    imageCapture!!.targetRotation = rotation
+                }
+            }
+            orientationEventListener.enable()
+
+            //画像分析のための処理.
+            imageAnalyzer = ImageAnalysis.Builder()
+                .build()
+                .also {
+                    it.setAnalyzer(cameraExecutor, LuminosityAnalyzer { luma ->
+                        Log.d(TAG, "Average luminosity: $luma")
+                    })
+                }
+
+            // Select back camera
+            val cameraSelector =
+                CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                preview?.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                camera = cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageCapture, imageAnalyzer
+                )
+            } catch (exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create timestamped output file to hold the image
+//        val photoFile = File(
+//            outputDirectory,
+//            SimpleDateFormat(FILENAME_FORMAT, Locale.US
+//            ).format(System.currentTimeMillis()) + ".jpg")
+        val imageFile = File.createTempFile("tmpMarTina"+
+                SimpleDateFormat(FILENAME_FORMAT, Locale.US
+            ).format(System.currentTimeMillis()), ".jpg")
+
+        // Create output options object which contains file + metadata
+        val outputOptions = ImageCapture.OutputFileOptions.Builder(imageFile).build()
+
+        //シャッター音追加のためのコード
+        val sound = MediaActionSound()
+        sound.load(MediaActionSound.SHUTTER_CLICK)
+
+        // Setup image capture listener which is triggered after photo has
+        // been taken
+        imageCapture.takePicture(
+            outputOptions, ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageSavedCallback {
+                override fun onError(exc: ImageCaptureException) {
+                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                }
+
+                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                    val savedUri = Uri.fromFile(imageFile)
+                    val msg = "Photo capture succeeded: $savedUri"
+                    //Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, msg)
+                    //シャッター音の追加
+                    sound.play(MediaActionSound.SHUTTER_CLICK)
+
+                    val intent = Intent(thisMainActivity, ResultActivity::class.java)
+                    intent.putExtra("imagePath", imageFile.path)
+                    startActivity(intent)
+                }
+
+            })
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+
+//    private fun getOutputDirectory(): File {
+//        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+//            File(it, resources.getString(R.string.app_name)).apply { mkdirs() } }
+//        val mediaDirS = mediaDir.toString()
+//        Log.d(TAG, "mediaDir: $mediaDirS filesDir:$filesDir" )
+//        return if (mediaDir != null && mediaDir.exists())
+//            mediaDir else filesDir
+//    }
+
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+    //画像分析のためのクラス
+    private class LuminosityAnalyzer(private val listener: LumaListener) : ImageAnalysis.Analyzer {
+
+        private fun ByteBuffer.toByteArray(): ByteArray {
+            rewind()    // Rewind the buffer to zero
+            val data = ByteArray(remaining())
+            get(data)   // Copy the buffer into a byte array
+            return data // Return the byte array
+        }
+
+        override fun analyze(image: ImageProxy) {
+
+            val buffer = image.planes[0].buffer
+            val data = buffer.toByteArray()
+            val pixels = data.map { it.toInt() and 0xFF }
+            val luma = pixels.average()
+
+            listener(luma)
+
+            image.close()
+        }
+    }
+
+}
---
+++ app/src/main/java/jp/ne/sppd/masuda/mar_vs_tina_app/ResultActivity.kt
@@ -0,0 +1,182 @@
+package jp.ne.sppd.masuda.mar_vs_tina_app
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Matrix
+import android.graphics.Rect
+import android.media.ExifInterface
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import jp.ne.sppd.masuda.mar_vs_tina_app.databinding.ActivityResultBinding
+import jp.ne.sppd.masuda.mar_vs_tina_app.ml.AutoModel072629368F16067259368
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.label.TensorLabel
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.io.File
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import kotlin.math.roundToInt
+
+
+class ResultActivity : AppCompatActivity(), Runnable {
+    private lateinit var binding: ActivityResultBinding
+    private val BATCH_SIZE = 1 //バッチサイズ
+    private val INPUT_PIXELS = 3 //入力ピクセル
+    private val IMAGE_SIZE = 480
+
+    private val imageBuffer = IntArray(IMAGE_SIZE * IMAGE_SIZE)
+    private lateinit var bitmap: Bitmap
+    private var path: String? = null
+    private val handler : Handler = Handler(Looper.getMainLooper())
+
+    // ラベルデータ
+    private var labels: List<String>? = null
+
+    // 入力
+    private var inBitmap: Bitmap? = null
+    private var inCanvas: Canvas? = null
+    private val inBitmapSrc: Rect = Rect()
+    private val inBitmapDst: Rect = Rect(0, 0, IMAGE_SIZE, IMAGE_SIZE)
+    private var inBuffer: ByteBuffer? = null
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // 渡された画像ファイルを削除する
+        val file = File(path!!)
+        file.delete()
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_result)
+
+        path = intent.getStringExtra("imagePath")
+        val exif = ExifInterface(path.toString())
+        val orientation: Int =
+            exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
+        bitmap = BitmapFactory.decodeFile(path)
+        val copiedBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val viewBitmap = rotateImage(copiedBitmap, orientation)
+
+        val viewFace = binding.viewImage //findViewById<ImageView>(R.id.view_picture)
+        viewFace.setImageBitmap(viewBitmap)
+
+        // 入力の初期化
+        inBitmap = Bitmap.createBitmap(
+            IMAGE_SIZE, IMAGE_SIZE, Bitmap.Config.ARGB_8888
+        )
+        inCanvas = Canvas(inBitmap!!)
+
+        val numBytesPerChannel = 4
+        inBuffer = ByteBuffer.allocateDirect(
+            BATCH_SIZE * IMAGE_SIZE * IMAGE_SIZE * INPUT_PIXELS * numBytesPerChannel
+        )
+        inBuffer!!.order(ByteOrder.nativeOrder())
+
+        // AI用のラベルデータ読込み
+        labels = FileUtil.loadLabels(this, "Labels.txt")
+
+        val resultView = binding.resultText //findViewById<TextView>(R.id.result_text)
+        resultView.text = "考え中・・・"
+
+        //handler.post(this)
+        handler.postDelayed(this, 100)
+    }
+
+    override fun run() {
+        //時間のかかる処理実行します。
+
+        // AI呼び出し
+        val resultList = predict(bitmap)
+
+        // 結果表示
+        val resultView = binding.resultText //findViewById<TextView>(R.id.result_text)
+        val percent = (resultList[0].second * 10000).roundToInt() /100
+        resultView.text = percent.toString() + "% " + resultList[0].first
+    }
+
+    private fun predict(bitmap: Bitmap): List<Pair<String, Float>> {
+        // 入力画像の生成
+        //inBitmapSrc.set(0, 0, bitmap.width, bitmap.height)
+        //inCanvas!!.drawBitmap(bitmap, inBitmapSrc, inBitmapDst, null)
+        inCanvas!!.drawBitmap(bitmap, null, inBitmapDst, null)
+
+        // 入力バッファの生成（画像）
+        bmpToInBuffer(inBitmap)
+
+        // 出力バッファの生成
+        var out: Array<FloatArray> = arrayOf(FloatArray(199))
+        val outputMap = mapOf(
+            0 to out
+        )
+
+        val model = AutoModel072629368F16067259368.newInstance(this)
+
+        // Creates inputs for reference.
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 480, 480, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(inBuffer!!)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        // Releases model resources if no longer used.
+        model.close()
+
+        /*
+        val model = Model.newInstance(this)
+
+        val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 224, 224, 3), DataType.FLOAT32)
+        inputFeature0.loadBuffer(inBuffer!!)
+
+        val outputs = model.process(inputFeature0)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+         */
+
+        // 結果取得
+        val results = TensorLabel(labels!!, outputFeature0!!)
+        val floatMap = results.mapWithFloatValue;
+        val resultList = floatMap.toList().sortedByDescending { it.second }.toList()
+
+        // 結果の取得
+        //return resultList[0].first
+        return resultList
+    }
+    private fun rotateImage(bitmap: Bitmap, orientation : Int): Bitmap? {
+        val degree = when (orientation) {
+            ExifInterface.ORIENTATION_ROTATE_90 -> 90.0
+            ExifInterface.ORIENTATION_ROTATE_180 -> 180.0
+            ExifInterface.ORIENTATION_ROTATE_270 -> 270.0
+            else -> 0.0
+        }
+        val matrix = Matrix()
+        matrix.postRotate(degree.toFloat())
+        val rotatedImg =
+            Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+        bitmap.recycle()
+        return rotatedImg
+    }
+
+
+    private fun bmpToInBuffer(bitmap: Bitmap?) {
+        inBuffer!!.rewind()
+        bitmap!!.getPixels(
+            imageBuffer, 0, bitmap.width,
+            0, 0, bitmap.width, bitmap.height
+        )
+        var pixel = 0
+        for (i in 0 until IMAGE_SIZE) {
+            for (j in 0 until IMAGE_SIZE) {
+                val pixelValue = imageBuffer[pixel++]
+                inBuffer!!.putFloat((pixelValue shr 16 and 0xFF) / 255.0f)
+                inBuffer!!.putFloat((pixelValue shr 8 and 0xFF) / 255.0f)
+                inBuffer!!.putFloat((pixelValue and 0xFF) / 255.0f)
+            }
+        }
+    }
+}
